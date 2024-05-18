07f3f3f08fad1c09da2d4e6763cc3e245b2dfe66
Anurag Singh <anuragvsingh999@gmail.com>
2024-05-18 17:16:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var button= document.getElementById('button1');
-    button.addEventListener('click', () =>{
-        var input= document.getElementById('input').value;
-        const sillycase=toSillyCase(input);
-        document.getElementById('input').value=sillycase;
-    })
+    var button1 = document.getElementById('button1');
+    var button2 = document.getElementById('button2');
+
+    button1.addEventListener('click', () => {
+        var input = document.getElementById('input').value;
+        const sillycase = toSillyCase(input);
+        document.getElementById('input').value = sillycase;
+    });
+
+    button2.addEventListener('click', () => {
+        var input = document.getElementById('input').value;
+        const reverseSillyCase = toReverseSillyCase(input);
+        document.getElementById('input').value = reverseSillyCase;
+    });
 });
-function toSillyCase(silly){
-    var sillycase=' ';
-    for( var i=0; i<silly.length; i++){
-        if(i%2==0){
-            sillycase +=silly[i].toLowerCase();
-        }else{
-            sillycase +=silly[i].toUpperCase();
+
+function toSillyCase(silly) {
+    var sillycase = '';
+    for (var i = 0; i < silly.length; i++) {
+        if (i % 2 === 0) {
+            sillycase += silly[i].toLowerCase();
+        } else {
+            sillycase += silly[i].toUpperCase();
         }
     }
     return sillycase;
 }
-document.addEventListener('DOMContentLoaded', function() {
-    var button= document.getElementById('button2');
-    button.addEventListener('click', () =>{
-        var input= document.getElementById('input').value;
-        const sillycase=toSillyCase(input);
-        document.getElementById('input').value=sillycase;
-    })
-});
-function toSillyCase(silly){
-    var sillycase=' ';
-    for( var i=0; i<silly.length; i++){
-        if(i%2==0){
-            reversesillycase +=silly[i].toUpperCase();
-        }else{
-            reversesillycase +=silly[i].toLowerCase();
+
+function toReverseSillyCase(silly) {
+    var reverseSillyCase = '';
+    for (var i = 0; i < silly.length; i++) {
+        if (i % 2 === 0) {
+            reverseSillyCase += silly[i].toUpperCase();
+        } else {
+            reverseSillyCase += silly[i].toLowerCase();
         }
     }
-    return sillycase;
+    return reverseSillyCase;
 }
